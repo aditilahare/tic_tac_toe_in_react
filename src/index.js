52d15ed25d4a43631a14984f21f953389e561b87
Aditi <in-aditi.lahare@in-aditilahare.local>
2019-03-14 12:52:05 +0530

    Rendered one button.

--- src/index.js
+++ src/index.js
@@ -2,23 +2,26 @@ import React from 'react';
 import ReactDOM from "react-dom";
 
 
-class Square extends React.Component{
- constructor(props){
-     super(props);
-     this.state = {
-         value: null
-     };
- }
 
- render() {
-     return(
-         "hello aditi"
-     )
- }
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        };
+    }
+
+    render() {
+        return (
+           "Hello world",
+               <button className='x' onClick={()=>{this.setState({value:'X'})}}> {this.state.value} </button>
+    );
+    }
+
 }
 
 
 ReactDOM.render(
-    <Square />,
-    document.getElementById('root')
-);
+    <Square/>,
+document.getElementById('root')
+)
