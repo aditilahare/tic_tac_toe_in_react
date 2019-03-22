b32c2f108eef5a3df3990cbbb1ef82129ae00f8a
Aditi <in-aditi.lahare@in-aditilahare.local>
2019-03-22 18:01:01 +0530

    Removed the redux code from app.

--- src/components/game.js
+++ src/components/game.js
@@ -38,13 +38,11 @@ class Board extends Component {
                     {this.renderSquare(1)}
                     {this.renderSquare(2)}
                 </div>
-
                 <div className='row-two'>
                     {this.renderSquare(3)}
                     {this.renderSquare(4)}
                     {this.renderSquare(5)}
                 </div>
-
                 <div className='row-three'>
                     {this.renderSquare(6)}
                     {this.renderSquare(7)}
